46a087691daa47d66f52d4498c57c6e4c5a35c59
Antonio <antoniofilhojsa@gmail.com>
2025-05-07 18:51:11 -0300

    add quick echo start

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
 func main(){
-	
+	if err := godotenv.Load(); err != nil{
+		log.Fatalf("Error loading env: %v", err)
+	} 
+	e := echo.New()
+	cfg := GetConfig()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, echo.Map{"message": "OK"})
+	})
+
+	e.Logger.Fatal(e.Start(cfg.Port))
 }
