b5ea90f97bc703fd09484d15e781a40bc15af96e
Antonio <antoniofilhojsa@gmail.com>
2025-05-08 15:00:04 -0300

    change name

---
+++ internal/interfaces/repository.go
@@ -0,0 +1,11 @@
+package interfaces
+
+import "github.com/DevAntonioJorge/go-blog/internal/models"
+
+type IUserRepository interface {
+	SaveUser(user *models.User) error
+	GetUserByEmail(email string) (*models.User, error)
+	GetUserByID(id string) (*models.User, error)
+	GetUserByName(name string) (*models.User, error)
+	UpdatePassword(email, password string) error
+}
