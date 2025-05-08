1f3d6b994eee1e609e78e0572ce8b5cc543e4725
Antonio <antoniofilhojsa@gmail.com>
2025-05-08 14:34:52 -0300

    add login and validate methods

--- internal/interfaces/user_usecases.go
+++ internal/interfaces/user_usecases.go
@@ -6,4 +6,6 @@ import (
 
 type IUserService interface{
 	SaveUser(user *models.User) error
+	Login(user *models.User, identifier string, idType string) error
+	ValidatePassword(user *models.User, password string) error
 }
