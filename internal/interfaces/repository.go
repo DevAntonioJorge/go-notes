3a8d0c8e788e51b75843b4e8ee85e1ed16d03151
Antonio <antoniofilhojsa@gmail.com>
2025-05-11 17:53:52 -0300

    feat: add INoteRepository and IFolderRepository interfaces for note and folder management

--- internal/interfaces/repository.go
+++ internal/interfaces/repository.go
@@ -9,3 +9,26 @@ type IUserRepository interface {
 	GetUserByName(name string) (*models.User, error)
 	UpdatePassword(user *models.User, password string) error
 }
+
+type INoteRepository interface {
+	SaveNote(note *models.Note) error
+	GetNote(id string) (*models.Note, error)
+	UpdateNote(id string, note *models.Note) error
+	DeleteNote(id string) error
+	GetNotes(userID string) ([]*models.Note, error)
+	SearchNotes(userID string, query string) ([]*models.Note, error)
+	GetNotesByFolder(folderID string) ([]*models.Note, error)
+    MoveNote(noteID string, newFolderID string) error
+    GetNotesByTag(userID string, tag string) ([]*models.Note, error)
+    GetRecentNotes(userID string, limit int) ([]*models.Note, error)
+}
+
+type IFolderRepository interface {
+	SaveFolder(folder *models.Folder) error
+    GetFolder(id string) (*models.Folder, error)
+    UpdateFolder(id string, folder *models.Folder) error
+    DeleteFolder(id string) error
+    GetFolders(userID string) ([]*models.Folder, error)
+    GetFolderByPath(userID string, path string) (*models.Folder, error)
+    MoveFolder(folderID string, newParentID string) error
+}
