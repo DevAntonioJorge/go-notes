e2baa2586cae6afbbdefc70022fb6fb74bddcbb3
Antonio <antoniofilhojsa@gmail.com>
2025-05-13 20:42:49 -0300

    folder name validation struct tag

--- internal/dto/folder.go
+++ internal/dto/folder.go
@@ -1,11 +1,11 @@
 package dto
 
 type CreateFolderRequest struct{
-	Name string `json:"name"`
+	Name string `json:"name" validate:"min=4,max=10"`
 	ParentID *string `json:"parent_id"`
 }
 type UpdateFolderRequest struct{
-	Name string `json:"name"`
+	Name string `json:"name" validate:"min=4,max=10"`
 }
 
 
