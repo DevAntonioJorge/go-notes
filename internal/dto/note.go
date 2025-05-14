4dea07f59493fdb538112e14e59743a91ee38e36
Antonio <antoniofilhojsa@gmail.com>
2025-05-13 21:28:51 -0300

    feat: add note validation

--- internal/dto/note.go
+++ internal/dto/note.go
@@ -3,8 +3,8 @@ package dto
 type CreateNoteRequest struct{
 	UserID string `json:"user_id"`
 	FolderID string `json:"folder_id"`
-	Title string `json:"title"`
-	Content string `json:"content"`
+	Title string `json:"title" validate:"required,min=4,max=20"`
+	Content string `json:"content" validate:"max=1000"`
 	Tags []string `json:"tags"`
 }
 
