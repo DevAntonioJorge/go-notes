1c8cca7a90268371c769331838e095c3f8454535
Antonio <antoniofilhojsa@gmail.com>
2025-05-15 10:26:37 -0300

    feat: add folder handlers

--- cmd/app/folder_handler.go
+++ cmd/app/folder_handler.go
@@ -9,67 +9,133 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type FolderHandler struct{
+type FolderHandler struct {
 	service interfaces.IFolderService
 }
 
-func NewFolderHandler(service interfaces.IFolderService) *FolderHandler{
+func NewFolderHandler(service interfaces.IFolderService) *FolderHandler {
 	return &FolderHandler{service}
 }
 
 func (h *FolderHandler) CreateFolderHandler(c echo.Context) error {
 	var req dto.CreateFolderRequest
 
-	if err := c.Bind(&req); err != nil{
+	if err := c.Bind(&req); err != nil {
 		return c.String(http.StatusBadRequest, "Invalid request body")
 	}
 
 	validate := validator.New()
-	if err := validate.Struct(req); err != nil{
+	if err := validate.Struct(req); err != nil {
 		return c.String(http.StatusInternalServerError, "Invalid folder name")
 	}
 
 	folder, err := h.service.SaveFolder(req)
-	if err != nil{
+	if err != nil {
 		return c.String(http.StatusInternalServerError, "Failed to save folder")
 	}
 
 	return c.JSON(http.StatusCreated, echo.Map{
 		"message": "Folder created",
-		"folder": folder,
+		"folder":  folder,
 	})
 }
 
-func (h *FolderHandler) GetFolderHandler(c echo.Context) error{
+func (h *FolderHandler) GetFolderHandler(c echo.Context) error {
 	var req dto.GetFolderRequest
-	if err := c.Bind(&req); err != nil{
+	if err := c.Bind(&req); err != nil {
 		return c.String(http.StatusBadRequest, "No id specified or invalid parameter")
 	}
 
 	folder, err := h.service.GetFolder(req.ID)
-	if err != nil{
+	if err != nil {
 		return c.String(http.StatusNotFound, "Folder not found")
 	}
 
-	return c.JSON(http.StatusAccepted, echo.Map{
+	return c.JSON(http.StatusOK, echo.Map{
 		"folder": folder,
 	})
 }
 
-func (h *FolderHandler) UpdateFolder(c echo.Context) error{
+func (h *FolderHandler) UpdateFolder(c echo.Context) error {
 	var req dto.UpdateFolderRequest
 
-	if err := c.Bind(&req); err != nil{
+	if err := c.Bind(&req); err != nil {
 		return c.String(http.StatusBadRequest, "Ïnvalid request body or invalid id")
 	}
 
 	folder, err := h.service.UpdateFolder(req)
-	if err != nil{
+	if err != nil {
 		return c.String(http.StatusInternalServerError, "Failed to update folder name")
 	}
 
 	return c.JSON(http.StatusAccepted, echo.Map{
 		"folder_id": req.ID,
+		"folder":    folder,
+	})
+}
+
+func (h *FolderHandler) DeleteFolderHandler(c echo.Context) error {
+	var req dto.DeleteFolderRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.String(http.StatusBadRequest, "Invalid id")
+	}
+
+	if err := h.service.DeleteFolder(req.ID); err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to delete folder")
+	}
+
+	return c.String(http.StatusAccepted, "Folder deleted")
+
+}
+func (h *FolderHandler) GetFoldersHandler(c echo.Context) error {
+	var req dto.GetFoldersRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.String(http.StatusBadRequest, "Invalid user id")
+	}
+
+	folderList, err := h.service.GetFolders(req.UserID)
+
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to get folders")
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"folder list": folderList,
+	})
+}
+
+func (h *FolderHandler) GetFolderByPathHandler(c echo.Context) error {
+	var req dto.GetFolderByPathRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.String(http.StatusBadRequest, "Invalid request body")
+	}
+
+	folder, err := h.service.GetFolderByPath(req.UserID, req.Path)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to get folder")
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"folder": folder,
+	})
+}
+
+func (h *FolderHandler) MoveFolder(c echo.Context) error {
+	var req dto.MoveFolderRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.String(http.StatusBadRequest, "Invalid request body")
+	}
+
+	folder, err := h.service.MoveFolder(req.FolderID, req.ParentID)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to move folder")
+	}
+
+	return c.JSON(http.StatusAccepted, echo.Map{
 		"folder": folder,
 	})
 }
--- internal/dto/folder.go
+++ internal/dto/folder.go
@@ -1,16 +1,32 @@
 package dto
 
-type CreateFolderRequest struct{
-	Name string `json:"name" validate:"min=4,max=10"`
+type CreateFolderRequest struct {
+	Name     string  `json:"name" validate:"min=4,max=10"`
 	ParentID *string `json:"parent_id"`
 }
-type UpdateFolderRequest struct{
-	ID string `param:"id"`
+type UpdateFolderRequest struct {
+	ID   string `param:"id"`
 	Name string `json:"name" validate:"min=4,max=10"`
 }
 
-type GetFolderRequest struct{
+type GetFolderRequest struct {
 	ID string `param:"id"`
 }
 
+type DeleteFolderRequest struct {
+	ID string `param:"id"`
+}
 
+type GetFoldersRequest struct {
+	UserID string `json:"user_id"`
+}
+
+type GetFolderByPathRequest struct {
+	UserID string `json:"user_id"`
+	Path   string `json:"path"`
+}
+
+type MoveFolderRequest struct {
+	FolderID string `json:"id"`
+	ParentID string `json:"id"`
+}
